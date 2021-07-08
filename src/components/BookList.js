09bf247b0d1328b813bcff2bc88fe2674420edb7
Eleazar Federio <eleazarfederio@rocketmail.com>
2021-07-08 09:23:39 +0800

    update book list component design

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,26 +1,31 @@
-import {Button, Card, Col} from "react-bootstrap";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 
 const {Component} = require("react");
 
 class BookList extends Component{
 
-
-
     render() {
         return (
             <Col lg={4} md={6} xs={12} className={'d-flex align-items-stretch'}>
                 <Card className={'mb-2 flex-fill'}>
-                    <Card.Img src={'https://veiindia.com/wp-content/uploads/2018/11/book-image-not-available.png'} />
+                
+                <img src="https://i.imgur.com/J5LVHEL.jpeg" class="rounded mx-auto d-block" alt="..."/>
                     <Card.Body>
-                        <p className={'text-center mb-0 mt-3'}>Title</p>
-                        <p className={'text-center mt-0'}><b>{this.props.title.substring(0, 40)}</b></p>
-                        <p className={'text-center mb-0'}>Author</p>
-                        <p className={'text-center mt-0'}><b>{this.props.author}</b></p>
-                        <p>Publisher: <b>{this.props.publisher}</b></p>
-                        <p>Pages: <b>{this.props.pages}</b></p>
-                        <p>Year: <b>{this.props.year}</b></p>
-                        <Button variant={'info'}>Borrow</Button>
+                        <Container>
+                            <p style={{marginBottom: 0}} className={'mb-0 mt-3'}>Title: <b>{this.props.title.substring(0, 40)}</b></p>
+                            <p style={{marginBottom: 0}}>Author: <b>{this.props.author}</b></p>
+                            {/*<p style={{marginBottom: 0}}>Publisher: <b>{this.props.publisher}</b></p>*/}
+                            <Row>
+                                <Col >
+                                    <p>Pages: <b>{this.props.pages}</b></p>
+                                </Col>
+                                <Col>
+                                    <p>Year: <b>{this.props.year}</b></p>
+                                </Col>
+                            </Row>
+                        </Container>
                     </Card.Body>
+                    <Button variant={'info'}>Borrow</Button>
                 </Card>
             </Col>
         );
